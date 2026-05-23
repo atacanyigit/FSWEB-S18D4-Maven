5a6fe15e311fe5dfeece03ebbf19db1a6c220476
Yiğit GÜLEÇ <githubda-kullandigin-email@example.com>
2026-05-23 11:24:01 +0300

    GlobalExceptionHandler file is changed

--- src/main/java/com/workintech/s18d1/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/workintech/s18d1/exceptions/GlobalExceptionHandler.java
@@ -1,11 +1,13 @@
 package com.workintech.s18d1.exceptions;
 
 import com.workintech.s18d1.exceptions.BurgerErrorResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
