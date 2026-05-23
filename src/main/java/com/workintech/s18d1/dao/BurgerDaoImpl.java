b418cd765bf947063da0c38f5a2c734597f0b1dd
Yiğit GÜLEÇ <githubda-kullandigin-email@example.com>
2026-05-23 11:16:12 +0300

    BurgerDaoImpl is up-to-date

--- src/main/java/com/workintech/s18d1/dao/BurgerDaoImpl.java
+++ src/main/java/com/workintech/s18d1/dao/BurgerDaoImpl.java
@@ -9,9 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-
+import lombok.extern.slf4j.Slf4j;
 import java.util.List;
 
+
+@Slf4j
 @Repository
 public class BurgerDaoImpl implements BurgerDao {
 
